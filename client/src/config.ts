acfd6e45409699f92f3a8b7a492872b349fe574d
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2020-09-04 21:54:10 -0400

    fix: remove hardcoded api host config

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,6 +5,5 @@ export type AppConfig = {
 
 const config: AppConfig = (window as any).APP_CONFIG
 
-// export const API_HOST = config.API_HOST
-export const API_HOST = "68.183.59.27:3000"
+export const API_HOST = config.API_HOST
 export const INPUT_SEND_RATE = Number(config.INPUT_SEND_RATE)
